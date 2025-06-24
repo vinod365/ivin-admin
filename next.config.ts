8405d5e308409d822a25844cce895ea7f99de27c
vinod <vinodtanwar495@gmail.com>
2025-06-25 00:17:06 +0530

    refactor: simplify SVG handling in webpack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "react-component",
-        },
-      },
-    },
-  },
+  webpack(config:any) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
